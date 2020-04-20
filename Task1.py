5c2850245c895237d105130831a161f63345bee4
NineMan <nelubinmv@inbox.ru>
2020-04-21 01:51:17 +0400

    Solved task1

--- Task1.py
+++ Task1.py
@@ -1,5 +1,18 @@
 class Point:
-    self.x
 
-    def dist(self, Point()):
-        s = se
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dist(self, point):
+        return ((point.x - self.x) ** 2 + (point.y - self.y) ** 2) ** 0.5
+
+
+p1 = Point(0, 0)
+p2 = Point(4, 3)
+print(p1.dist(p2))
+print(p1.dist(p1))
+print(p2.dist(p1))
+print(p2.dist(p2))
+
+
